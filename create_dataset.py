af275c53e60510f847ab436de9dc54af271b42be
laragamasantos <laragamasantos@gmail.com>
2025-08-19 09:34:43 -0300

    create file

---
+++ create_dataset.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+def generate_dataset(num_sales, start_date, end_date):
+    id = np.arange(1,num_sales)
+    
+    dates = []
+    for _ in range(num_sales):
+        
+
+if __name__ == "__main__":
+    num_sales = 50
+    start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")"01/01/2023"
+    end_date = "31/12/2023"
